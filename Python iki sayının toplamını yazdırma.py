113d605b1ef0334e81239befbee9ee589b5669d0
xR0use <75970877+xR0use@users.noreply.github.com>
2021-02-08 23:33:00 +0300

    Add files via upload

---
+++ Python iki sayının toplamını yazdırma.py
@@ -0,0 +1,12 @@
+
+
+
+num1 = int(input("birinci sayıyı giriniz :"))
+num2 = int(input("ikinci sayıyı giriniz :"))
+
+
+# iki sayının toplamı
+sum = int(num1) + float(num2)
+
+# sonucu yazdırma
+print('{0} + {1} = {2}'.format(num1, num2, sum))
